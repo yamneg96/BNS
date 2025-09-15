f510ef3e522f426a21c087e6cd49e3dc2a53b70e
Chala Temesgen <ctemesgen85@gmail.com>
2025-09-15 04:44:28 -0300

    Get all departments and  Get single department api added

---
+++ Backend/controllers/departmentController.js
@@ -0,0 +1,22 @@
+import Department from "../models/Department.js";
+
+// Get all departments
+export const getDepartments = async (req, res) => {
+  try {
+    const departments = await Department.find();
+    res.json(departments);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get single department
+export const getDepartment = async (req, res) => {
+  try {
+    const dept = await Department.findById(req.params.id);
+    if (!dept) return res.status(404).json({ message: "Department not found" });
+    res.json(dept);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
