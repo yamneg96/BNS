9150853fbf58082f86a95d80d818af15cc49ccf0
Yamlak Negash <yamlaknegash96@gmail.com>
2025-09-16 00:24:52 +0300

    Removed unwanted Components.

--- Frontend/src/components/DepCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DepCard = () => {
-  return (
-    <div>DepCard</div>
-  )
-}
-
-export default DepCard
--- Frontend/src/components/WardCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WardCard = () => {
-  return (
-    <div>WardCard</div>
-  )
-}
-
-export default WardCard
