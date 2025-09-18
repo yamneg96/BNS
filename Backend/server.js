615446cd87c8e92a9c5cd3e6bd2c0d984906bd10
Yamlak Negash <yamlaknegash96@gmail.com>
2025-09-19 00:18:42 +0300

    safety for port call added

--- Backend/server.js
+++ Backend/server.js
@@ -29,5 +29,5 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process?.env?.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
