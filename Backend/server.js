ee8f7560a429eb4acfdd843891291a39b9c7bb75
Yamlak Negash <yamlaknegash96@gmail.com>
2025-09-19 00:12:08 +0300

    corrected frontend url for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 
