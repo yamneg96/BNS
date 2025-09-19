0445466d05f1ca53aff9d2f6b32081fcc7e2a811
Yamlak Negash <yamlaknegash96@gmail.com>
2025-09-19 16:33:04 +0300

    corrected naming of imports.

--- Backend/routes/notificationRoutes.js
+++ Backend/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getNotificationsForUser } from "../controllers/NotificationController.js";
+import { getNotificationsForUser } from '../controllers/NotificationController.js'
 
 const router = express.Router();
 
