c9870b502d825d12488b3444458e08e800ea8ca4
Chala Temesgen <ctemesgen85@gmail.com>
2025-09-19 06:18:22 -0300

    Get logged-in user's assignments done

--- Backend/routes/assignmentRoutes.js
+++ Backend/routes/assignmentRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   createAssignment,
-getAssignmentExpiryForUser
+getAssignmentExpiryForUser,
+getMyAssignments
 } from "../controllers/assignmentController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -9,6 +10,8 @@ const router = express.Router();
 
 router.post("/", protect, createAssignment);
 router.get("/user/:userId/expiry", protect, getAssignmentExpiryForUser);
+// Protected route: any logged-in user can view their assignments
+router.get("/my", protect, getMyAssignments);
 
 
 
