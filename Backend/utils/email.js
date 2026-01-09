9d2391ce270b0a074113bda16270e8889f893b15
Chala Temesgen <ctemesgen85@gmail.com>
2026-01-09 16:05:19 +0300

    cheking the expiry function

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -4,13 +4,17 @@ export const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      port: Number(process.env.SMTP_PORT),
       secure: false,
+      requireTLS: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
       },
     });
+
+     await transporter.verify();
+     console.log(" SMTP connection verified");
 
     const htmlTemplate = `
       <!DOCTYPE html>
