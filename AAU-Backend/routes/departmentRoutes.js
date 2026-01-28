cbd67bf0ce9d5f93d41b5056d4e7c5d6a261a5b1
Chala Temesgen <ctemesgen85@gmail.com>
2026-01-28 23:32:45 +0300

    get all dept routed

---
+++ AAU-Backend/routes/departmentRoutes.js
@@ -0,0 +1,19 @@
+
+import express from "express";
+import { getDepartments, } from "../controllers/departmentController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+
+router.get("/", getDepartments);
+//router.get("/:id", getDepartment);
+//router.get("/department/:deptId/wards/:wardName/beds/:bedId/patient", protect, getBedPatient);
+//router.get("/department/:deptId/wards/:wardName/beds/:bedId/patient/history", protect, getBedPatientHistory);
+//router.post("/admit", protect, admitPatient);
+//router.post("/patient", protect, recordPatientInBed);
+//router.post("/discharge", protect, dischargePatient);
+//router.put("/update-patient", protect, updatePatientInBed);
+
+export default router;
