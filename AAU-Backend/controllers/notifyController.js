9c245fe20441b82b5f0c093b3dcf36bc522f7297
Chala Temesgen <ctemesgen85@gmail.com>
2026-02-02 14:59:28 +0300

    getNotificationsForUser added

---
+++ AAU-Backend/controllers/notifyController.js
@@ -0,0 +1,31 @@
+import Notification from "../models/Notification.js";
+
+export const getNotificationsForUser = async (req, res) => {
+    try {
+        const {
+            departmentName,
+            wardName,
+            roomNumber,
+            bedNumber,
+            read,
+        } = req.query;
+
+        const query = { user: req.user._id };
+
+        // Room-aware filters
+        if (departmentName) query.departmentName = departmentName;
+        if (wardName) query.wardName = wardName;
+        if (roomNumber) query.roomNumber = roomNumber;
+        if (bedNumber) query.bedNumber = Number(bedNumber);
+        if (read !== undefined) query.read = read === "true";
+
+        const notifications = await Notification.find(query)
+            .populate("from", "name email image")
+            .sort({ createdAt: -1 })
+            .limit(50);
+
+        res.json(notifications);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
